a879903b5e764fbe085d8ca25e841a9c9725c95c
Brandon <brandonbgrayson@gmail.com>
2022-10-15 08:38:16 -0400

    Project Basemodel added and request sent from postman

--- main.py
+++ main.py
@@ -1,8 +1,16 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
 
 app = FastAPI()
 
+class Project(BaseModel):
+    title: str
+    description: str
+    open_positions: list
+    link_to_repo: str
+
+
 
 @app.post("/add-project")
-async def getAll():
-    return {"project": "some project data"}
+async def getAll(project: Project):
+    return {"project": project}
