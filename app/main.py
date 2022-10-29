48c4017e600492634896e0984cbef88eebaf8d45
Brandon <brandonbgrayson@gmail.com>
2022-10-28 20:02:10 -0400

    Cors middleware

--- app/main.py
+++ app/main.py
@@ -3,8 +3,19 @@ from pydantic import BaseModel
 import psycopg2
 from psycopg2.extras import RealDictCursor
 from .config import settings
+from fastapi.middleware.cors import CORSMiddleware
+
+origins = ['http://localhost']
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins = origins,
+    allow_credentials = True,
+    allow_methods=["*"],
+    allow_headers=["*"]
+)
 try: 
     conn = psycopg2.connect(host=settings.database_hostname, database=settings.database_name, user=settings.database_username, password=settings.database_password, cursor_factory=RealDictCursor)
     cursor = conn.cursor()
