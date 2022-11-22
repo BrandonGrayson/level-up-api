8ae3d6ebbfbc2830ba367424c465038e9ce51f14
Brandon <brandonbgrayson@gmail.com>
2022-11-21 23:39:42 -0500

    added secret key to env variables

--- app/config.py
+++ app/config.py
@@ -6,6 +6,7 @@ class Settings(BaseSettings):
     database_password: str
     database_name: str
     database_username: str
+    secret_key: str
 
     class Config:
         env_file = '.env'
--- app/oauth2.py
+++ app/oauth2.py
@@ -1,7 +1,8 @@
 from jose import JWTError, jwt
 from datetime import datetime, timedelta
+from .config import settings
 
-SECRET_KEY = "09d2e39d095b57be27d5a4940ebcd6ec81e6bbcc5839d2c4b6f02207a15a84d0"
+SECRET_KEY = settings.secret_key
 
 ALGORITHM = "HS256"
 ACCESS_TOKEN_EXPIRE_MINUTES = 30
