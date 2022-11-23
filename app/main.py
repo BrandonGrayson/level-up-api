5dffb4e1ad63e5f02bef2b402074c8b5b37c8768
Brandon <brandonbgrayson@gmail.com>
2022-11-23 00:18:47 -0500

    authentication bug fixes

--- app/main.py
+++ app/main.py
@@ -12,18 +12,19 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins = origins,
-    allow_credentials = True,
+    allow_origins=origins,
+    allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"]
 )
 
-try: 
-    conn = psycopg2.connect(host=settings.database_hostname, database=settings.database_name, user=settings.database_username, password=settings.database_password, cursor_factory=RealDictCursor)
+try:
+    conn = psycopg2.connect(host=settings.database_hostname, database=settings.database_name,
+                            user=settings.database_username, password=settings.database_password, cursor_factory=RealDictCursor)
     cur = conn.cursor()
     print("database connection successful")
 
-except Exception as error: 
+except Exception as error:
     print("connection to database failed")
     print("Error", error)
 
@@ -32,19 +33,21 @@ except Exception as error:
 async def root():
     return {"message": "Hello Bigger Applications!"}
 
+
 @app.get("/projects")
 async def getAllProjects():
     cur.execute(""" SELECT * FROM projects """)
     projects = cur.fetchall()
     return {'data': projects}
 
+
 @app.post("/projects")
 async def addProject(new_project: schemas.NewProject):
-    cur.execute(""" INSERT INTO projects (title, description, open_positions, link_to_repo) VALUES (%s, %s, %s, %s) RETURNING * """, (new_project.title, new_project.description, new_project.open_positions, new_project.link_to_repo)) 
+    cur.execute(""" INSERT INTO projects (title, description, open_positions, link_to_repo) VALUES (%s, %s, %s, %s) RETURNING * """,
+                (new_project.title, new_project.description, new_project.open_positions, new_project.link_to_repo))
     project_details = cur.fetchone()
     conn.commit()
     return {"new_project": project_details}
-
 
 
 @app.post("/users", status_code=status.HTTP_201_CREATED, response_model=schemas.UserOut)
@@ -53,39 +56,43 @@ async def newUser(user: schemas.UserCreate):
     hashed_password = utils.hashPassword(user.password)
     user.password = hashed_password
 
-    cur.execute(""" INSERT INTO users (email, password) VALUES (%s, %s) RETURNING * """, (user.email, user.password))
+    cur.execute(""" INSERT INTO users (email, password) VALUES (%s, %s) RETURNING * """,
+                (user.email, user.password))
     user_details = cur.fetchone()
     conn.commit()
 
-    return user_details 
+    return user_details
+
 
 @app.get("/users/{id}", response_model=schemas.UserOut)
 async def getUser(id: int, ):
     cur.execute(""" SELECT * FROM users WHERE id = %s """, (str(id),))
 
     project = cur.fetchone()
-    
+
     if not project:
-        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"user with id: {id} does not exist")
-    
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
+                            detail=f"user with id: {id} does not exist")
+
     return project
+
 
 @app.get("/login")
 async def login(user_credentials: OAuth2PasswordRequestForm = Depends()):
 
-    cur.execute(""" SELECT * FROM users WHERE email = (%s) """, (user_credentials.username,))
+    cur.execute(""" SELECT * FROM users WHERE email = (%s) """,
+                (user_credentials.username,))
 
     user = cur.fetchone()
 
     if not user:
-        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"Invalid Credentials")
+        raise HTTPException(
+            status_code=status.HTTP_403_FORBIDDEN, detail=f"Invalid Credentials")
 
     if not utils.verifyPassword(user_credentials.password, user['password']):
-        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"Invalid Credentials")
+        raise HTTPException(
+            status_code=status.HTTP_403_FORBIDDEN, detail=f"Invalid Credentials")
 
     access_token = oauth2.create_access_token(data={"user_id": user['id']})
 
     return {"access_token": access_token, "token_type": "bearer"}
-
-
-    
--- app/oauth2.py
+++ app/oauth2.py
@@ -15,7 +15,7 @@ ACCESS_TOKEN_EXPIRE_MINUTES = 30
 def create_access_token(data: dict):
     to_encode = data.copy()
 
-    expire = datetime.now() + timedelta(ACCESS_TOKEN_EXPIRE_MINUTES)
+    expire = datetime.utcnow() + timedelta(ACCESS_TOKEN_EXPIRE_MINUTES)
     to_encode.update({"exp": expire})
 
     encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
@@ -26,7 +26,7 @@ def create_access_token(data: dict):
 def verify_access_token(token: str, credentials_exception):
 
     try:
-        payload = jwt.decode(token, SECRET_KEY, ALGORITHM)
+        payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
         id = payload.get("user_id")
         if id is None:
             raise credentials_exception
@@ -35,6 +35,8 @@ def verify_access_token(token: str, credentials_exception):
     except JWTError:
         raise credentials_exception
 
+    return token_data
+
 
 async def get_current_user(token: str = Depends(oauth2_scheme)):
     credentials_exception = HTTPException(
@@ -42,3 +44,5 @@ async def get_current_user(token: str = Depends(oauth2_scheme)):
         detail=f"Could not validate credentials",
         headers={"WWW-Authenticate": "Bearer"}
     )
+
+    return verify_access_token(token, credentials_exception)
