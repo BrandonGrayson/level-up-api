9fa27b0726480249d3f48f6cf0e0c713c896f3d0
Brandon <brandonbgrayson@gmail.com>
2022-11-23 00:05:58 -0500

    get_current_user function

--- app/oauth2.py
+++ app/oauth2.py
@@ -2,10 +2,15 @@ from jose import JWTError, jwt
 from datetime import datetime, timedelta
 from .config import settings
 from . import schemas
+from fastapi import Depends, status, HTTPException
+from fastapi.security import OAuth2PasswordBearer
+
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl='/login')
 
 SECRET_KEY = settings.secret_key
 ALGORITHM = "HS256"
 ACCESS_TOKEN_EXPIRE_MINUTES = 30
+
 
 def create_access_token(data: dict):
     to_encode = data.copy()
@@ -17,15 +22,23 @@ def create_access_token(data: dict):
 
     return encoded_jwt
 
+
 def verify_access_token(token: str, credentials_exception):
 
     try:
-       payload = jwt.decode(token, SECRET_KEY, ALGORITHM)
-       id = payload.get("user_id")
-       if id is None:
-        raise credentials_exception
-    
-       token_data = schemas.TokenData(id=id)
+        payload = jwt.decode(token, SECRET_KEY, ALGORITHM)
+        id = payload.get("user_id")
+        if id is None:
+            raise credentials_exception
+        token_data = schemas.TokenData(id=id)
+
     except JWTError:
         raise credentials_exception
 
+
+async def get_current_user(token: str = Depends(oauth2_scheme)):
+    credentials_exception = HTTPException(
+        status_code=status.HTTP_401_UNAUTHORIZED,
+        detail=f"Could not validate credentials",
+        headers={"WWW-Authenticate": "Bearer"}
+    )
